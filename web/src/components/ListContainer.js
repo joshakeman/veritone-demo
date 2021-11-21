5d3e2699dfd08c58a691d36b8dd226d6ef232165
Josh Akeman <joshakeman@gmail.com>
2021-11-21 10:43:14 -0500

    created appbar

---
+++ web/src/components/ListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+export default function ListContainer() {
+    return (
+        <Box sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}>
+            <Button>Save</Button>
+        </Box>
+    )
+}
